4ed450ef6e481c9b812e43973726cc1089733b54
Loc Nguyen <locnguyen@MacBook-Pro.local>
2018-07-26 15:46:55 -0400

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
@@ -64,8 +66,8 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
 });
 
 // 1. Make new template that will render when the client visits the root of the website.
