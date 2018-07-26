ec2a838003fe1ae464afdb2083039e67ad96602c
Loc Nguyen <locnguyen@MacBook-Pro.local>
2018-07-26 15:55:22 -0400

    removed maintenance page

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-})
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// })
 
 app.use((req, res, next) => {
   var now = new Date().toString();
